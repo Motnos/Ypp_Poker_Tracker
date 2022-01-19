8b0423cbfd9afd8041cefe3ba0069a91872bb20c
Motnos <45239114+Motnos@users.noreply.github.com>
2022-01-19 23:30:11 +0000

    Update Poker Tracker.ps1
    Included rebuys

--- Poker Tracker.ps1
+++ Poker Tracker.ps1
@@ -1,10 +1,12 @@
-$logLocation = 'D:\Ypp Chat Logging\Ept_emerald_Ypp_Chat_Log_EPT - Copy.txt' #Set this to the location of your Ypp Chat Log
+$logLocation = 'D:\Ypp Chat Logging\Ept_emerald_Ypp_Chat_Log_EPT2.txt' #Set this to the location of your Ypp Chat Log
 $pirateName = 'ept' #Set this to your Pirate Name
 
 $totalBuyIn = 0
+$totalRebuy = 0
 $totalCashOut = 0
 
 $buyIns = Select-String -Path $logLocation -Pattern "$pirateName bought in for ([\d]*)" 
+$rebuys = Select-String -Path $logLocation -Pattern "$pirateName rebought for ([\d]*)"
 $cashOuts = Select-String -Path $logLocation -pattern "Ye cashed out with ([\d]*)"
 
 foreach ($buyIn in $buyIns) {
@@ -15,10 +17,14 @@ foreach ($cashOut in $cashOuts) {
     $cashOut -match "Ye cashed out with ([\d,]*)" | Out-Null
     $totalCashOut = $totalCashout + $Matches[1]
 }
+foreach ($rebuy in $reBuys) {
+    $rebuy -match "rebought for ([\d,]*)" | Out-Null
+    $totalRebuy = $totalRebuy + $Matches[1]
+}
 
-$finalValue = $totalCashOut - $totalBuyIn
+$finalValue = $totalCashOut - $totalBuyIn -$totalRebuy
 
-If ($totalCashOut -ge $totalBuyIn) {
+If ($finalValue -ge 0) {
 Write-Host "You made a profit of $finalValue PoE" -ForegroundColor Green
 }
 Else {
