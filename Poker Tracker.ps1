1b69e694c2c2946653e9b06e2abc0b4aee89dc0a
Motnos <45239114+Motnos@users.noreply.github.com>
2022-01-23 21:47:32 +0000

    Update Poker Tracker.ps1
    Fixed bug where it was missing some rebuys

--- Poker Tracker.ps1
+++ Poker Tracker.ps1
@@ -1,28 +1,35 @@
-$logLocation = 'C:\Ypp Chat Logs\Location\File.txt' #Set this to the location of your Ypp Chat Log
-$pirateName = 'PirateName' #Set this to your Pirate Name
+$logLocation = 'D:\Ypp Chat Logging\Ept_emerald_Ypp_Chat_Log_EPT.txt' #Set this to the location of your Ypp Chat Log
+$pirateName = 'Ept' #Set this to your Pirate Name
 
 $totalBuyIn = 0
 $totalRebuy = 0
 $totalCashOut = 0
+$totalCashOutRebuy = 0
 
-$buyIns = Select-String -Path $logLocation -Pattern "$pirateName bought in for ([\d]*)" 
-$rebuys = Select-String -Path $logLocation -Pattern "$pirateName rebought for ([\d]*)"
-$cashOuts = Select-String -Path $logLocation -pattern "Ye cashed out with ([\d]*)"
+$buyIns = Select-String -Path $logLocation -Pattern "[[]{1}[\d]{2}[:]{1}[\d]{2}[:]{1}[\d]{2}\S{1} $pirateName bought in for ([\d]*)" 
+$rebuys = Select-String -Path $logLocation -Pattern "[[]{1}[\d]{2}[:]{1}[\d]{2}[:]{1}[\d]{2}\S{1} $pirateName rebought for ([\d]*)"
+$cashOuts = Select-String -Path $logLocation -pattern "[[]{1}[\d]{2}[:]{1}[\d]{2}[:]{1}[\d]{2}\S{1} Ye cashed out with ([\d]*)"
+$cashOutRebuys = Select-String -Path $logLocation -pattern "[[]{1}[\d]{2}[:]{1}[\d]{2}[:]{1}[\d]{2}\S{1} $pirateName cashed out with ([\d]*)" # If you cash out and rebuy in the same turn puts your pirate name in the chat rather than 'Ye'.
 
 foreach ($buyIn in $buyIns) {
     $buyIn -match "bought in for ([\d,]*)" | Out-Null
     $totalBuyin = $totalBuyin + $Matches[1]
 }
-foreach ($cashOut in $cashOuts) {
-    $cashOut -match "Ye cashed out with ([\d,]*)" | Out-Null
-    $totalCashOut = $totalCashout + $Matches[1]
-}
 foreach ($rebuy in $reBuys) {
     $rebuy -match "rebought for ([\d,]*)" | Out-Null
     $totalRebuy = $totalRebuy + $Matches[1]
 }
+foreach ($cashOut in $cashOuts) {
+    $cashOut -match "Ye cashed out with ([\d,]*)" | Out-Null
+    $totalCashOut = $totalCashout + $Matches[1]
+}
+foreach ($cashOutRebuy in $cashOutRebuys) {
+    $cashOutRebuy -match "$pirateName cashed out with ([\d,]*)" | Out-Null
+    $totalCashOutRebuy = $totalCashoutRebuy + $Matches[1]
+}
 
-$finalValue = $totalCashOut - $totalBuyIn -$totalRebuy
+$totalCashOut = $totalCashOut + $totalCashOutRebuy
+$finalValue = $totalCashOut - $totalBuyIn -$totalRebuy 
 
 If ($finalValue -ge 0) {
     Write-Host "Total Bought In In $totalBuyIn" -ForegroundColor Red
